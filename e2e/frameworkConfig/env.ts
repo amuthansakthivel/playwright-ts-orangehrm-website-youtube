6b221472f39d28c1b7e09f84b47cb07f3541bba4
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2024-03-17 19:52:01 +0530

    feat: added create employee test

---
+++ e2e/frameworkConfig/env.ts
@@ -0,0 +1,5 @@
+export class Env {
+  public static readonly BASE_URL = process.env.URL;
+  public static readonly USERNAME = process.env.USERNAME;
+  public static readonly PASSWORD = process.env.PASSWORD;
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,32 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import { config } from "dotenv";
 
+//staging ==> .env.staging
+if (process.env.ENVIRONMENT) {
+  console.log("ENVIRONMENT: ", process.env.ENVIRONMENT);
+  config({
+    path: `.env.${process.env.ENVIRONMENT}`,
+    override: true,
+  });
+} else {
+  config();
+}
 
 export default defineConfig({
-  testDir: './e2e/tests',
+  testDir: "./e2e/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: "html",
   use: {
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    }
-  ]
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
 });
